1effe8c529d8ed761c28d928a052806df9cf11cb
Ryuuz02 <55858331+Ryuuz02@users.noreply.github.com>
2023-09-25 20:57:53 -0500

    Deleted duplicate directions field

--- db/migrations/001-init.sql
+++ db/migrations/001-init.sql
@@ -12,7 +12,6 @@ CREATE TABLE "User" (
 CREATE TABLE Recipe (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
-  instructions TEXT NOT NULL,
   averageCalories INT,
   prepTimeSeconds INT,
   imageUrl TEXT,
